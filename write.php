429ce027b0123c7274f0e93c8e8c2d8ba539352b
sarath <sarath@sarath.(none)>
2013-12-30 11:11:39 +0530

    data updated

--- write.php
+++ write.php
@@ -1,9 +1,13 @@
 <?php
+session_start();
+mkdir("data");
 $out=file_get_contents('php://input');
+//$out=file_get_contents('output');
 //$out=$_POST['output'];
-$file = fopen("test.txt","a+");
+$file = fopen("./data/test".$_SESSION['count'].".txt","w");
+//$file = fopen("./data/test.txt","a+");
 fwrite($file,$out);
 fclose($file);
-
+$_SESSION['count']+=1;
 
 ?>
